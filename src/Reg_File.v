60edf49bde62fd22616d395353f09325674a1ae4
Balaji Srinivasan <93860462+pyCoder03@users.noreply.github.com>
2024-07-11 23:05:17 +0530

    Update Reg_File.v

--- src/Reg_File.v
+++ src/Reg_File.v
@@ -1,25 +1,5 @@
 
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.04.2024 16:18:42
-// Design Name: 
-// Module Name: Reg_File
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Reg_File(
     input [31:0] reg_w,
